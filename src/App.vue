b765c325b790c3abcf808e0c9a7f348efe6465a7
Erik <eb.dietrich11@gmail.com>
2021-09-07 21:00:52 -0500

    switch computers

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,62 @@
 <template>
   <div id="app">
     <div class="menuPane">
-      <Menu
-        :sections="sections"
-        :current="current"
-        :setSection="setSection"
-      />
+      <Menu :sections="sections" :current="current" :setSection="setSection" />
     </div>
     <div class="mainPane">
-      <component
-        :is="current"
-        v-bind="sections[current].props"
-      />
+      <component :is="current" v-bind="sections[current].props" />
     </div>
   </div>
 </template>
 
 <script>
-import Home from './components/Home.vue';
-import Resume from './components/Resume.vue';
-import Menu from './components/Menu.vue';
-import Cribbage from './components/Cribbage.vue';
+import Home from "./components/Home.vue";
+import Resume from "./components/Resume.vue";
+import Menu from "./components/Menu.vue";
+import Cribbage from "./components/Cribbage.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   data() {
     return {
-      current : 'home',
+      current: "home",
       resume: {
         info: {
-          name: 'Erik Dietrich',
-          email: 'eb.dietrich11@gmail.com',
-          phone: '9991234567',
-          location: 'Madison, WI'
-        }
+          name: "Erik Dietrich",
+          email: "eb.dietrich11@gmail.com",
+          phone: "9991234567",
+          location: "Madison, WI",
+        },
       },
-    }
+    };
   },
   components: {
     Home,
     Resume,
     Menu,
-    Cribbage
+    Cribbage,
   },
   computed: {
     sections() {
       return {
-        home: { name: 'Home', key: 'home', template: '<App />', props: {} },
-        resume: { name: 'Resume', key: 'resume', template: '<Resume />', props: { resume: this.resume } },
-        cover: { name: 'Cover', key: 'cover', template: '<Cover />' },
-        personalInfo: { name: 'Personal Info', key: 'personalInfo', template: '<Personal />' },
-        cribbage: { name: 'Cribbage Counter', key: 'cribbage', template: '<Cribbage />' }
+        home: { name: "Home", key: "home", template: "<App />", props: {} },
+        resume: {
+          name: "Resume",
+          key: "resume",
+          template: "<Resume />",
+          props: { resume: this.resume },
+        },
+        cover: { name: "Cover", key: "cover", template: "<Cover />" },
+        personalInfo: {
+          name: "Personal Info",
+          key: "personalInfo",
+          template: "<Personal />",
+        },
+        cribbage: {
+          name: "Cribbage Counter",
+          key: "cribbage",
+          template: "<Cribbage />",
+        },
       };
     },
   },
@@ -59,7 +65,7 @@ export default {
       this.current = section;
     },
   },
-}
+};
 </script>
 
 <style>
@@ -71,21 +77,26 @@ export default {
   color: black;
   display: flex;
   flex-flow: wrap;
+  padding: 10px;
 }
 .menuPane {
   width: 100%;
-  margin: 20px 10px;
+  margin: 20px 0px;
   border-bottom: 2px solid #60789e;
 }
+.mainPane {
+  width: 100%;
+}
 .button {
-  color: black;
+  color: white;
+  background-color: #60789e;
   border-radius: 8px;
-  padding: 5px;
+  padding: 3px;
   border: 1px solid #60789e;
 }
 .button:hover {
-    color: white;
-    background-color: #60789e;
-    cursor: pointer;
+  color: white;
+  background-color: #425677;
+  cursor: pointer;
 }
 </style>
