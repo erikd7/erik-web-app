e5b945fe89dd9b6e56f4a568872aa29df38ccf7c
Erik <eb.dietrich11@gmail.com>
2021-11-26 12:41:25 -0500

    Start to flesh out home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,21 +1,28 @@
 <template>
     <div>
-        <div class="header">
-            Hello, welcome to my website.
-        </div>
+        <CardTile header="Front End">
+            JavaScript Vue.js Framework
+        </CardTile>
+        <CardTile header="Back End">
+            Python Flask Framework
+        </CardTile>
+        <CardTile header="DevOps">
+            GitHub and Docker
+        </CardTile>
     </div>
 </template>
 
 <script>
-export default {
-  props: {
-    },
-  }
+    import CardTile from '../SharedComponents/CardTile.vue';
+    export default {
+        components: { CardTile },
+        props: {},
+    };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.header {
-    font-weight: bold;
-}
+    .header {
+        font-weight: bold;
+    }
 </style>
