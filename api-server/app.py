7c35e35ec2f3b09095b2a78628472f2e98c0d58e
Erik <eb.dietrich11@gmail.com>
2021-12-09 19:40:54 -0600

    Set docker to run waitress

--- api-server/app.py
+++ api-server/app.py
@@ -17,4 +17,4 @@ if __name__ == "__main__":
   port = int(os.environ.get('PORT', 49155))
   print('Initializing and running app on port ' + str(port))
   #app.run(debug=True, host="0.0.0.0", port=port, use_reloader=False)
-  serve(app.app, host='0.0.0.0', port=port)
+  serve(app, host='0.0.0.0', port=port)
