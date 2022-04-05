95cb56833be3641ef93db84e04cd78d7bb5d11e0
Erik <eb.dietrich11@gmail.com>
2022-04-05 18:02:29 -0500

    Cribbage points should tally correctly within class

--- src/components/Cribbage.vue
+++ src/components/Cribbage.vue
@@ -1,137 +1,70 @@
 <template>
-  <div class="flex flex-col">
-    <div class="p-b-10">
-      enter points placeholder
-      <button class="button" @click="calculatePoints">
-        Count Points
-      </button>
+    <div class="flex flex-col">
+        <div class="p-b-10">
+            enter points placeholder
+            <button class="button" @click="calculatePoints">
+                Count Points
+            </button>
+        </div>
+        <div v-if="this.cardsResult.totalPoints"
+            >Total Points is {{ this.cardsResult.totalPoints }}</div
+        >
+        <div
+            v-if="this.cardsResult.totalPoints"
+            class="flex flex-row justify-items-center justify-evenly flex-wrap"
+        >
+            <Card
+                cardTitle="Sums"
+                cardLabel="Sum"
+                :cards="cardsResult.sums.cards"
+                :points="cardsResult.sums.points"
+            />
+            <Card
+                cardTitle="Pairs"
+                cardLabel="Pair"
+                :cards="cardsResult.pairs.cards"
+                :points="cardsResult.pairs.points"
+            />
+            <Card
+                cardTitle="Runs"
+                cardLabel="Run"
+                :cards="cardsResult.runs.cards"
+                :points="cardsResult.runs.points"
+            />
+        </div>
     </div>
-    <div v-if="totalPoints">Total Points is {{ totalPoints }}</div>
-    <div
-      v-if="totalPoints"
-      class="flex flex-row justify-items-center justify-evenly flex-wrap"
-    >
-      <Card
-        cardTitle="Sums"
-        cardLabel="Sum"
-        :cards="cardsResult.sums.cards"
-        :points="cardsResult.sums.points"
-      />
-      <Card
-        cardTitle="Pairs"
-        cardLabel="Pair"
-        :cards="cardsResult.pairs.cards"
-        :points="cardsResult.pairs.points"
-      />
-      <Card
-        cardTitle="Runs"
-        cardLabel="Run"
-        :cards="cardsResult.runs.cards"
-        :points="cardsResult.runs.points"
-      />
-    </div>
-  </div>
 </template>
 
 <script>
-import Card from "../SharedComponents/Card.vue";
-import { helpers, CardResult } from "../util/pointsCounterHelpers";
+    import Card from '../SharedComponents/Card.vue';
+    import { CardResult } from '../util/pointsCounterHelpers';
 
-export default {
-  components: { Card },
-  props: {
-    resume: {
-      type: Object,
-      default: () => {},
-    },
-  },
-  data() {
-    return {
-      cardValues: [8, 5, 5, 5, 6, 7, 8, 9],
-      cardFaces: ["8", "Q", "K", "2", "5", "5", "5", "6", "7", "8", "J"],
-      total: 15,
-      totalPoints: 0,
-      cardsResult: CardResult.build(),
+    export default {
+        components: { Card },
+        props: {
+            resume: {
+                type: Object,
+                default: () => {},
+            },
+        },
+        data() {
+            return {
+                cardFaces: ['7', '5', '5', '6', '8'],
+                cardsResult: CardResult.build(['7', '5', '5', '6', '8']),
+            };
+        },
+        methods: {
+            calculatePoints() {
+                this.cardsResult = CardResult.build(['7', '5', '5', '6', '8']);
+                this.cardsResult.calculatePoints();
+            },
+        },
     };
-  },
-  methods: {
-    calculatePoints() {
-      this.cardsResult = CardResult.build();
-      //this.getSumPoints(this.cardValues, this.total)
-      //const cardsValueMap = this.createCardHash(this.cardValues)
-      const cardsFaceMap = helpers.createCardHash(this.cardFaces);
-      this.getPairPoints(cardsFaceMap);
-      this.getRunPoints(cardsFaceMap);
-      this.totalPoints = 10;
-      console.log("end", this.cardsResult);
-    },
-    getPairPoints(cardsMap) {
-      let numCombos = 0;
-      Object.values(cardsMap).forEach(
-        (p) => (numCombos += helpers.getNumCombos(p))
-      );
-      let pairCards = [];
-      Object.keys(cardsMap).forEach((key) => {
-        const numCards = cardsMap[key].length;
-        if (numCards >= 2) {
-          let toPush = {};
-          toPush[key] = numCards;
-          pairCards.push(toPush);
-        }
-      });
-      this.cardsResult.pairs.points += 2 * numCombos;
-      this.cardsResult.pairs.cards = pairCards;
-    },
-    getRunPoints(cardsMap) {
-      const minRunLength = 3;
-      let runs = {};
-      let starterCard = "x";
-      //Loop through facecards and check for next in sequence
-      helpers.orderedFaces.concat(["z"]).forEach((face) => {
-        //add dummy to last card to count runs ending with lastcard. refactor later
-        const cardInHand = cardsMap[face];
-
-        if (cardInHand) {
-          if (starterCard === "x") {
-            //If it's first in a sequence
-            starterCard = face;
-            runs[face] = [face];
-          } else {
-            //If part of existing sequence
-            runs[starterCard].push(face);
-          }
-        } else {
-          if (starterCard !== "x" && runs[starterCard].length >= minRunLength) {
-            //If end of run, document cards and points
-            this.cardsResult.runs.cards.push(runs[starterCard]);
-            this.cardsResult.runs.points += runs[starterCard].length;
-          }
-          starterCard = "x";
-        }
-      });
-      console.log("runs is", runs);
-      console.log("runs result is", this.cardsResult.runs.cards);
-    },
-    /*getSumPoints(cards, target, cardsMap) {
-        console.log('cards map is ', cardsMap, target   )
-        for (const topCardIndex in cards) {
-            const topCard = cards[topCardIndex]
-            console.log('topcardindex is', topCardIndex)
-            console.log('cards is', cards)
-            console.log('sliced cards is', cards.slice(topCardIndex + 1))
-            for (const nextCardIndex in cards.slice(topCardIndex + 1)) {
-                const nextCard = cards[nextCardIndex]
-            }
-        }
-      },*/
-    cardsToFaceValues() {},
-  },
-};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.header {
-  font-weight: bold;
-}
+    .header {
+        font-weight: bold;
+    }
 </style>
--- src/util/pointsCounterHelpers.js
+++ src/util/pointsCounterHelpers.js
@@ -75,6 +75,31 @@ const factorial = (number) => {
   return total;
 };
 
+const waysToSum = (numbers, sumTo, numbersSoFar = [], successfulNumbers = [], level = 0) => {
+  const locNumbers = cloneDeep(numbers);
+  const locNumbersSoFar = cloneDeep(numbersSoFar);
+  for (let index = 0; index < locNumbers.length; index++) {
+    const loopNumbersSoFar = cloneDeep(locNumbersSoFar)
+    const n = locNumbers[index]
+
+    //track the current traversal
+    loopNumbersSoFar.push(n)
+
+    //new value to sum to
+    const newSumTo = sumTo - n;
+
+    //if it adds to new value
+    if (newSumTo === 0) {      
+      successfulNumbers.push(loopNumbersSoFar);
+    } else if (newSumTo > 0 && locNumbers.length) {
+      //use recursion to evaluate next numbers
+      waysToSum(locNumbers.slice(index+1),newSumTo, loopNumbersSoFar, successfulNumbers, level + 1);
+    } 
+  }
+
+  return successfulNumbers;
+}
+
 export const helpers = {
   orderedFaces,
   isLastCard,
@@ -82,20 +107,119 @@ export const helpers = {
   createCardHash,
   getNumCombos,
   factorial,
+  waysToSum
 };
+
 export class CardResult {
-  constructor(pairs, runs, sums) {
+  constructor(faces, pairs, runs, sums) {
+    this.faces = faces;
     this.pairs = pairs;
     this.runs = runs;
     this.sums = sums;
   }
 
-  static build() {
+  createCardHash() {
+    //Create hash of card -> index in cards array
+    let cardsMap = {};
+    this.faces.forEach((card, index) => {
+      const current = cardsMap[card] ? cardsMap[card] : [];
+      cardsMap[card] = current.concat(index);
+    });
+    this.cardsMap = cardsMap;
+  }
+
+  getCardValues() {
+    this.values = this.faces.map(f => facesToValues[f])
+  }
+
+  static build(cardFaces) {
     const blankInsideObject = { cards: [], points: 0 };
-    return new CardResult(
+    const created = new CardResult(
+      cardFaces,
       cloneDeep(blankInsideObject),
       cloneDeep(blankInsideObject),
       cloneDeep(blankInsideObject)
     );
+
+    created.createCardHash();
+    created.getCardValues();
+
+    return created;
+  }
+
+  getSumPoints() {
+    const sums = waysToSum(this.values, 15);
+    this.sums.points += 2 * sums.length;
+    this.sums.cards = sums;
+  }
+
+  getRunPoints() {
+    const minRunLength = 3;
+    let runs = {};
+    let dups = {};
+    let starterCard = null;
+    //Loop through all faces and check for next in sequence
+    helpers.orderedFaces.concat(["z"]).forEach((face) => {
+      //add dummy to last card to count runs ending with lastcard. refactor later
+      const cardsInHand = this.cardsMap[face];
+  
+      if (cardsInHand) {
+        if (starterCard === null) {
+          //If it's first in a sequence
+          starterCard = face;
+          runs[face] = [face];
+        } else {
+          //If part of existing sequence
+          runs[starterCard].push(face);
+        }
+        if (cardsInHand.length > 1) {
+          dups[face] = cardsInHand.length - 1;
+        }
+      } 
+      //If end of run, document cards and points
+      else {
+        if (starterCard && runs[starterCard].length >= minRunLength) {
+          this.runs.cards.push(runs[starterCard]);
+          //Include duplicate runs
+          this.runs.points += runs[starterCard].length;
+          runs[starterCard].forEach(c => {
+            const runDups = dups[c];
+            for (let i = 0; i < runDups; i++) {
+              this.runs.cards.push(runs[starterCard]);
+              this.runs.points += runs[starterCard].length;
+            }
+          })
+          
+          
+        }
+        starterCard = null;
+      }
+    });
+  }
+
+  getPairPoints() {
+    let numCombos = 0;
+    Object.values(this.cardsMap).forEach(
+      (p) => (numCombos += helpers.getNumCombos(p))
+    );
+    let pairCards = [];
+    Object.keys(this.cardsMap).forEach((key) => {
+      const numCards = this.cardsMap[key].length;
+      if (numCards >= 2) {
+        let toPush = {};
+        toPush[key] = numCards;
+        pairCards.push(toPush);
+      }
+    });
+    this.pairs.points += 2 * numCombos;
+    this.pairs.cards = pairCards;
+  }
+
+  calculatePoints() {
+    this.getSumPoints();
+    this.getPairPoints();
+    this.getRunPoints();
+
+    this.totalPoints = this.sums.points + this.pairs.points + this.runs.points;
   }
 }
