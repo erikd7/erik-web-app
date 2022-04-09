41f7d29463ccd78c2d79a8b202fbe3cdc70e66b5
Erik <eb.dietrich11@gmail.com>
2022-04-09 08:03:28 -0500

    Slot body and header in CardTile

--- src/SharedComponents/CardTile.vue
+++ src/SharedComponents/CardTile.vue
@@ -1,65 +1,68 @@
 <template>
-    <div class="card" :class="{ 'hover-grow': hoverGrow }" style="--scale: 1.01">
-        <div v-if="header" :class="`card-header ${headerClass}`">
-            {{ header }}
-            <div v-if="subHeader" class="card-subheader">
-                {{ subHeader }}
-            </div>
-        </div>
-        <div>
-            <slot></slot>
-        </div>
+  <div class="card" :class="{ 'hover-grow': hoverGrow }" style="--scale: 1.01">
+    <div v-if="header" :class="`card-header ${headerClass}`">
+      {{ header }}
+      <div v-if="subHeader" class="card-subheader">
+        {{ subHeader }}
+      </div>
     </div>
+    <div v-else>
+      <slot name="header" class="card-header"></slot>
+    </div>
+    <div>
+      <slot name="body"></slot>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {};
-        },
-        props: {
-            header: {
-                type: String,
-                default: '',
-            },
-            headerClass: {
-                type: String,
-                default: '',
-            },
-            subHeader: {
-                type: String,
-                default: '',
-            },
-            hoverGrow: {
-                type: Boolean,
-                default: true,
-            },
-        },
-    };
+export default {
+  data() {
+    return {};
+  },
+  props: {
+    header: {
+      type: String,
+      default: '',
+    },
+    headerClass: {
+      type: String,
+      default: '',
+    },
+    subHeader: {
+      type: String,
+      default: '',
+    },
+    hoverGrow: {
+      type: Boolean,
+      default: true,
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
-    .card {
-        padding: 5px;
-        margin: 5px;
-        border-radius: 10px;
-        background-color: #c3cfe9;
-        text-align: center;
-        transition: all 0.15s ease-in-out;
-    }
-    .card-title {
-        font-weight: bold;
-        border-bottom: 1px solid #425677;
-    }
-    .card-title > div {
-        padding: 0px 5px;
-    }
-    .card-header {
-        border-bottom: 1px solid grey;
-        font-size: larger;
-    }
-    .card-subheader {
-        font-size: small;
-    }
+.card {
+  padding: 5px;
+  margin: 5px;
+  border-radius: 10px;
+  background-color: #c3cfe9;
+  text-align: center;
+  transition: all 0.15s ease-in-out;
+}
+.card-title {
+  font-weight: bold;
+  border-bottom: 1px solid #425677;
+}
+.card-title > div {
+  padding: 0px 5px;
+}
+.card-header {
+  border-bottom: 1px solid grey;
+  font-size: larger;
+}
+.card-subheader {
+  font-size: small;
+}
 </style>
--- src/components/About.vue
+++ src/components/About.vue
@@ -2,54 +2,69 @@
     <div>
         <div class="flex flex-row justify-center flex-nowrap mobile-one-col">
             <CardTile header="About this App" headerClass="text-3xl">
-                I made this as a personal project to continue learning, house existing and
-                future projects, and enjoy hobby coding!
-                <CardTile header="Resume" :hoverGrow="false">
-                    My web resume
-                </CardTile>
-                <CardTile header="Chess" :hoverGrow="false">
-                    A player-vs-player chess application (in progress)
-                </CardTile>
-                <CardTile header="Cribbage" :hoverGrow="false">
-                    A points counter for the game of Cribbage (really in progress)
-                </CardTile>
+                <template v-slot:body>
+                    I made this as a personal project to continue learning, house existing
+                    and future projects, and enjoy hobby coding!
+                    <CardTile header="Resume" :hoverGrow="false"
+                        ><template v-slot:body> My web resume</template>
+                    </CardTile>
+                    <CardTile header="Chess" :hoverGrow="false"
+                        ><template v-slot:body>
+                            A player-vs-player chess application (in progress)</template
+                        >
+                    </CardTile>
+                    <CardTile header="Cribbage" :hoverGrow="false"
+                        ><template v-slot:body>
+                            A points counter for the game of Cribbage (really in
+                            progress)</template
+                        >
+                    </CardTile>
+                </template>
             </CardTile>
-            <CardTile header="About the Tech Stack" headerClass="text-3xl">
-                <CardTile header="Front End" :hoverGrow="false">
-                    <ul class="list-disc list-inside text-left">
-                        <li>JavaScript</li>
-                        <li>Vue.js Framework</li>
-                        <li>Tailwind CSS Framework</li>
-                        <li>Axios.js API Framework</li>
-                    </ul>
-                </CardTile>
-                <CardTile header="Back End - Python" :hoverGrow="false">
-                    <ul class="list-disc list-inside text-left">
-                        <li>Python API Server</li>
-                        <li>Pip Package Management</li>
-                        <li>Flask API Framework</li>
-                        <li>Gunicorn WSGI Server Framework</li>
-                        <li>Postman for testing</li>
-                    </ul>
-                </CardTile>
-                <CardTile header="DevOps" :hoverGrow="false">
-                    <ul class="list-disc list-inside text-left">
-                        <li>Git Source Control via GitHub</li>
-                        <li>Hosting</li>
-                        <li style="margin-left:2em">
-                            JS Front End
-                        </li>
-                        <li style="margin-left:4em">
-                            Auto deploy to Vercel via GitHub Actions
-                        </li>
-                        <li style="margin-left:2em">
-                            Python Back End
-                        </li>
-                        <li style="margin-left:4em">
-                            Auto deploy to Heroku via GitHub Actions
-                        </li>
-                    </ul>
-                </CardTile>
+            <CardTile header="About the Tech Stack" headerClass="text-3xl"
+                ><template v-slot:body>
+                    <CardTile header="Front End" :hoverGrow="false"
+                        ><template v-slot:body>
+                            <ul class="list-disc list-inside text-left">
+                                <li>JavaScript</li>
+                                <li>Vue.js Framework</li>
+                                <li>Tailwind CSS Framework</li>
+                                <li>Axios.js API Framework</li>
+                            </ul></template
+                        >
+                    </CardTile>
+                    <CardTile header="Back End - Python" :hoverGrow="false"
+                        ><template v-slot:body>
+                            <ul class="list-disc list-inside text-left">
+                                <li>Python API Server</li>
+                                <li>Pip Package Management</li>
+                                <li>Flask API Framework</li>
+                                <li>Gunicorn WSGI Server Framework</li>
+                                <li>Postman for testing</li>
+                            </ul></template
+                        >
+                    </CardTile>
+                    <CardTile header="DevOps" :hoverGrow="false"
+                        ><template v-slot:body>
+                            <ul class="list-disc list-inside text-left">
+                                <li>Git Source Control via GitHub</li>
+                                <li>Hosting</li>
+                                <li style="margin-left:2em">
+                                    JS Front End
+                                </li>
+                                <li style="margin-left:4em">
+                                    Auto deploy to Vercel via GitHub Actions
+                                </li>
+                                <li style="margin-left:2em">
+                                    Python Back End
+                                </li>
+                                <li style="margin-left:4em">
+                                    Auto deploy to Heroku via GitHub Actions
+                                </li>
+                            </ul></template
+                        >
+                    </CardTile></template
+                >
             </CardTile>
         </div>
     </div>
