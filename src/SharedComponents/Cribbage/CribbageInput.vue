ce6fb8cd89ab6bc6737163307c0a3da30d9c1c1f
Erik <eb.dietrich11@gmail.com>
2022-05-21 17:10:28 -0500

    Create UI for cribbage card input

---
+++ src/SharedComponents/Cribbage/CribbageInput.vue
@@ -0,0 +1,42 @@
+<template>
+  <span class="grid grid-cols-4 gap-4">
+    <div
+      v-for="option in options"
+      :key="option.value"
+      class="mx-auto flex flex-col justify-center bg-white rounded-2xl shadow-xl shadow-slate-300/60"
+      :class="{
+        'hover-grow cursor-pointer': !option.disabled,
+        'opacity-20': option.disabled,
+      }"
+    >
+      <PlayingCard
+        :cardFace="option.value"
+        :title="option.value"
+        class="max-w-60 w-28 object-cover"
+        :disabled="option.disabled"
+        :onSelect="() => selectFunction(option.value)"
+      />
+    </div>
+  </span>
+</template>
+
+<script>
+import PlayingCard from './PlayingCard.vue';
+
+export default {
+  data() {
+    return {};
+  },
+  components: { PlayingCard },
+  props: {
+    options: {
+      type: Array,
+      default: () => [],
+    },
+    selectFunction: {
+      type: Function,
+      default: () => {},
+    },
+  },
+};
+</script>
---
+++ src/SharedComponents/Cribbage/PlayingCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <img
+    :src="require(`@/data/cards/${cardFace === 'AD' ? 'AceD' : cardFace}.svg`)"
+    @click="
+      () => {
+        if (!disabled) onSelect();
+      }
+    "
+  />
+  <!-- Loading image titled 'AD' can be blocked by some ad blockers -->
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  props: {
+    cardFace: {
+      type: String,
+      default: '',
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    onSelect: {
+      type: Function,
+      default: () => {},
+    },
+  },
+};
+</script>
