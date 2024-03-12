41f68e9838ca2682289b24f9dfeade54a602c32e
erikd7 <eb.dietrich11@gmail.com>
2024-03-12 13:59:52 -0400

    Display core stack for resume experience

--- src/SharedComponents/TagArray.vue
+++ src/SharedComponents/TagArray.vue
@@ -1,35 +1,39 @@
 <template>
-    <div class="flex flex-row flex-wrap justify-left">
-        <Tag
-            v-for="(item, index) in array"
-            :key="index"
-            :label="item[labelKey]"
-            :tooltip="item[tooltipKey]"
-        />
-    </div>
+  <div :class="`flex flex-row flex-wrap ${containerClass}`">
+    <Tag
+      v-for="(item, index) in array"
+      :key="index"
+      :label="item[labelKey]"
+      :tooltip="item[tooltipKey]"
+    />
+  </div>
 </template>
 
 <script>
-    import Tag from './Tag';
+import Tag from "./Tag";
 
-    export default {
-        components: { Tag },
-        data() {
-            return {};
-        },
-        props: {
-            array: {
-                type: Array,
-                default: () => [],
-            },
-            labelKey: {
-                type: String,
-                default: 'label',
-            },
-            tooltipKey: {
-                type: String,
-                default: 'tooltip',
-            },
-        },
-    };
+export default {
+  components: { Tag },
+  data() {
+    return {};
+  },
+  props: {
+    array: {
+      type: Array,
+      default: () => []
+    },
+    labelKey: {
+      type: String,
+      default: "label"
+    },
+    tooltipKey: {
+      type: String,
+      default: "tooltip"
+    },
+    containerClass: {
+      type: String,
+      default: "justify-left"
+    }
+  }
+};
 </script>
